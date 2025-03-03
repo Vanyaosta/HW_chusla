ad383736c1e6974403b112be118a04f6b347b233
user <ivanostaschuk@gmail.com>
2025-03-03 21:10:29 +0200

    done task

---
+++ chusla_stovp4ik.py
@@ -0,0 +1,11 @@
+
+chuslo = int(input("Enter 4-digit number: "))
+perwe = chuslo // 1000
+druge = chuslo // 100 % 10
+tretie = chuslo // 10 % 10
+chetverte = chuslo % 10
+
+print(perwe)
+print(druge)
+print(tretie)
+print(chetverte)
---
+++ zadomnapered.py
@@ -0,0 +1,12 @@
+chuslo = int(input("Enter 5-digit number: "))
+perwe = chuslo // 10000
+druge = chuslo // 1000 % 10
+tretie = chuslo // 100 % 10
+chetverte = chuslo // 10 % 10
+piate = chuslo % 10
+
+print(piate)
+print(chetverte)
+print(tretie)
+print(druge)
+print(perwe)
